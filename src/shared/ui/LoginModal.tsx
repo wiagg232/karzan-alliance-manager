86fcc0dcd58c367f579473e7ed06a79e6faab859
ChaosOp <tp6ru8bj6@gmail.com>
2026-03-22 15:05:02 +0800

    fix: add skipBrowserRedirect: false,  flowType: 'implicit' to signInWithOAuth

--- src/shared/ui/LoginModal.tsx
+++ src/shared/ui/LoginModal.tsx
@@ -21,6 +21,8 @@ export function LoginModal({ onClose }: { onClose: () => void }) {
                 options: {
                     redirectTo: `${window.location.origin}${window.location.pathname}#/`,
                     scopes: 'guilds.members.read',
+                    skipBrowserRedirect: false,
+                    flowType: 'implicit'
                 },
             });
 
